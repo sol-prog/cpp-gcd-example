b4289d539d23e22e9ad2159cc37ac0d3c53ce2d2
Paul Silisteanu <solarianprogrammer@gmail.com>
2019-10-30 06:28:27 -0400

    Delete gcd_4.cpp

--- gcd_4.cpp
+++
@@ -1,9 +0,0 @@
-// Example of using the standard library gcd implementation
-#include <numeric>
-#include <array>
-#include <iostream>
-
-int main() {
-    std::array data{-13, 26, -39, 13};
-    std::cout << "gcd(data) = " << std::reduce(data.begin(), data.end(), 0, std::gcd<int,int>) << " Expected : 13\n";
-}
